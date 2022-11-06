c07546e67351ebabfb4982a3abc79efe53a98e15
David M. Chen <chen7david@me.com>
2022-11-07 01:45:32 +0800

    setup for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "http://127.0.0.1:5500/",
+  base: "/vite-ts-playground/",
   plugins: [react()],
 });
